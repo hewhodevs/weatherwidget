bde1903f42f743f055fbc85532f8e599c3ab3a96
hewhodevs <hewhodevs@outlook.com>
2021-04-06 21:55:02 +0930

    created component folder and basic setup

---
+++ src/Components/WeatherWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WeatherWidget.css';
+
+const WeatherWidget = () => {
+  return (
+    <div className="WeatherWidget">
+      <p>widget here!</p>
+    </div>
+  );
+}
+
+export default WeatherWidget;
